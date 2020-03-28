f48c572d62b9f9ca70fe1471c767d7e3d3cb539d
vradoskein <joaorener@gmail.com>
2020-03-28 14:39:37 -0300

    Adjusting all buttons, need to make them fit the screen.

--- Paint/src/SwingPaint.java
+++ Paint/src/SwingPaint.java
@@ -10,8 +10,9 @@ import javax.swing.JPanel;
 
 public class SwingPaint {
 
-    JButton clearBtn, blackBtn, blueBtn, greenBtn, redBtn, magentaBtn;
-    JButton squareBtn;
+    JButton clearBtn, blackBtn, floodBtn, saveBtn, openBtn;
+    JButton squareBtn, transBtn, rotBtn, escalaBtn, reflexBtn;
+    JButton ddaBtn, bresenhamBtn, circBtn, cohenBtn, liangBtn;
     DrawArea drawArea;
     ActionListener actionListener = new ActionListener() {
 
@@ -20,14 +21,6 @@ public class SwingPaint {
                 drawArea.clear();
             } else if (e.getSource() == blackBtn) {
                 drawArea.black();
-            } else if (e.getSource() == blueBtn) {
-                drawArea.blue();
-            } else if (e.getSource() == greenBtn) {
-                drawArea.green();
-            } else if (e.getSource() == redBtn) {
-                drawArea.red();
-            } else if (e.getSource() == magentaBtn) {
-                drawArea.magenta();
             } else if (e.getSource() == squareBtn) {
                 drawArea.square();
             }
@@ -59,7 +52,7 @@ public class SwingPaint {
         // add to content pane
         content.add(controls, BorderLayout.NORTH);
 
-        frame.setSize(800, 600);
+        frame.setSize(1600, 600);
         // can close frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // show the swing paint result
@@ -71,29 +64,53 @@ public class SwingPaint {
     private void createPanel(JPanel controls) {
         
         // create controls to apply colors and call clear feature
+        saveBtn = new JButton("Save");
+        saveBtn.addActionListener(actionListener);
+        openBtn = new JButton("Open");
+        openBtn.addActionListener(actionListener);
         clearBtn = new JButton("Clear");
         clearBtn.addActionListener(actionListener);
         blackBtn = new JButton("Black");
         blackBtn.addActionListener(actionListener);
-        blueBtn = new JButton("Blue");
-        blueBtn.addActionListener(actionListener);
-        greenBtn = new JButton("Green");
-        greenBtn.addActionListener(actionListener);
-        redBtn = new JButton("Red");
-        redBtn.addActionListener(actionListener);
-        magentaBtn = new JButton("Magenta");
-        magentaBtn.addActionListener(actionListener);
-        squareBtn = new JButton("Square");
+        squareBtn = new JButton("retângulo");
         squareBtn.addActionListener(actionListener);
-
+        ddaBtn = new JButton("DDA");
+        ddaBtn.addActionListener(actionListener);
+        bresenhamBtn = new JButton("Bresenham");
+        bresenhamBtn.addActionListener(actionListener);
+        circBtn = new JButton("Circunferência");
+        circBtn.addActionListener(actionListener);
+        transBtn = new JButton("Translacao");
+        transBtn.addActionListener(actionListener);
+        rotBtn = new JButton("Rotacao");
+        rotBtn.addActionListener(actionListener);
+        escalaBtn = new JButton("Escala");
+        escalaBtn.addActionListener(actionListener);
+        reflexBtn = new JButton("Reflexao");
+        reflexBtn.addActionListener(actionListener);
+        cohenBtn = new JButton(" Cohen-Sutherland");
+        cohenBtn.addActionListener(actionListener);
+        floodBtn = new JButton("Flood Fill");
+        floodBtn.addActionListener(actionListener);
+        
+        
+        
+        
         // add to panel
-        controls.add(greenBtn);
-        controls.add(blueBtn);
-        controls.add(blackBtn);
-        controls.add(redBtn);
-        controls.add(magentaBtn);
+        controls.add(saveBtn);
+        controls.add(openBtn);
         controls.add(clearBtn);
         controls.add(squareBtn);
+        controls.add(ddaBtn);
+        controls.add(bresenhamBtn);
+        controls.add(circBtn);
+        controls.add(transBtn);
+        controls.add(rotBtn);
+        controls.add(escalaBtn);
+        controls.add(reflexBtn);
+        controls.add(cohenBtn);
+        controls.add(floodBtn);
+        
     }
 
 }

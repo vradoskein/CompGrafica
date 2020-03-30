70c676fcb59bab991ca18afc54eea3bbe9f81b38
vradoskein <joaorener@gmail.com>
2020-03-30 18:54:31 -0300

    Creating the file to the algorithms, just the structure. Need to make the implementation.

---
+++ Paint/src/Implementation.java
@@ -0,0 +1,64 @@
+/*
+ * Class for the algorithms
+ */
+
+/**
+ *
+ * @author vradoskein
+ */
+public class Implementation {
+
+    public static void save() {
+        //TODO
+    }
+
+    public static void open() {
+        //TODO
+    }
+
+    public static void clear() {
+        //TODO
+    }
+
+    public static void square() {
+        //TODO
+        System.out.println("This is a test");
+    }
+
+    public static void dda() {
+        //TODO
+    }
+
+    public static void bresenham() {
+        //TODO
+    }
+
+    public static void circ() {
+        //TODO
+    }
+
+    public static void trans() {
+        //TODO
+    }
+
+    public static void rot() {
+        //TODO
+    }
+
+    public static void escala() {
+        //TODO
+    }
+
+    public static void reflexao() {
+        //TODO
+    }
+
+    public static void cohen() {
+        //TODO
+    }
+
+    public static void flood() {
+        //TODO
+    }
+
+}

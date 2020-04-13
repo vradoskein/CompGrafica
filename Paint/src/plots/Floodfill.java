8108bc9c2caafa56cbc954a6c764868318acc115
vradoskein <joaorener@gmail.com>
2020-04-13 15:06:16 -0300

    Creating the logic to save and open images.

---
+++ Paint/src/plots/Floodfill.java
@@ -0,0 +1,17 @@
+package plots;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author vradoskein
+ */
+public class Floodfill {
+
+    public static void plot(Point p1, Graphics2D g) {
+
+
+    }
+}

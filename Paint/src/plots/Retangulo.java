e1840beac5d8be9c92b5be377e6a20d2b29b69e6
vradoskein <joaorener@gmail.com>
2020-04-17 12:06:11 -0300

    Ordering the rectangle points

--- Paint/src/plots/Retangulo.java
+++ Paint/src/plots/Retangulo.java
@@ -9,16 +9,43 @@ import obj.Objeto;
  * @author Pedro
  */
 public class Retangulo {
+
     public static void plot(Objeto o, Graphics2D g) {
         Point p1, p2, p3, p4;
+        Point aux;
         p1 = o.getPontos().get(0);
         p2 = o.getPontos().get(1);
         p3 = o.getPontos().get(2);
         p4 = o.getPontos().get(3);
         
-        DDA.plot(p1, p3, g);
-        DDA.plot(p3, p2, g);
-        DDA.plot(p2, p4, g);
-        DDA.plot(p4, p1, g);
+        System.out.println(
+                "[RETANGULO CLASS BEFORE]" + "[P1] :" + p1 + "[P2] :" + p2 + "[P3] :" + p3 + "[P4] :" + p4);
+
+        if(p1.y > p3.y ){
+            aux = p1;
+            p1 = p3;
+            p3 = aux;
+        }
+        
+        if(p2.y < p4.y){
+            aux = p2;
+            p2 = p4;
+            p4 = aux;
+                    
+        }
+        
+
+        
+
+        System.out.println(
+                "[RETANGULO CLASS AFTER]" + "[P1] :" + p1 + "[P2] :" + p2 + "[P3] :" + p3 + "[P4] :" + p4);
+
+        DDA.plot(p1, p3, g);//
+
+        DDA.plot(p2, p3, g);
+
+        DDA.plot(p2, p4, g);//
+
+        DDA.plot(p1, p4, g);
     }
 }

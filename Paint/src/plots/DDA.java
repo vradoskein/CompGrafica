897ee23b5ee917c3fcf5cb6d0cfd8b7c1fda5258
vradoskein <joaorener@gmail.com>
2020-04-18 17:15:08 -0300

    Adjusting parallel dda

--- Paint/src/plots/DDA.java
+++ Paint/src/plots/DDA.java
@@ -15,6 +15,7 @@ import obj.Objeto;
 public class DDA {
 
     static Point p1, p2, pmedio;
+    static Point p1dividido, p2dividido;
 
     public static void plot(Objeto o, Graphics2D g) {
         g.setStroke(new BasicStroke(5));
@@ -24,51 +25,61 @@ public class DDA {
         p2 = o.getPontos().get(1);
         System.out.println("[DDA PLOTING] [p1] :" + p1 + "[p2]" + p2);
 
-        int mx = (p1.x + p2.x) / 2;
-        int my = (p1.y + p2.y) / 2;
-        pmedio = new Point(mx, my);
-        
+        pmedio = new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+        p1dividido = new Point((p1.x + pmedio.x) / 2, (p1.y + pmedio.y) / 2);
+        p2dividido = new Point((pmedio.x + p2.x) / 2, (pmedio.y + p2.y) / 2);
+
         System.out.println("[DDA PLOTING] [PONTO MEDIO] :" + pmedio);
         //plot(p1, p2, g);
 
-        ParallelDDA tarefa1 = new ParallelDDA(p1, pmedio, g);
-        ParallelDDA tarefa2 = new ParallelDDA(pmedio, p2, g);
+        ParallelDDA tarefa1 = new ParallelDDA(p1, p1dividido, g, 1);
+        ParallelDDA tarefa2 = new ParallelDDA(p1dividido, pmedio, g, 2);
+        ParallelDDA tarefa3 = new ParallelDDA(pmedio, p2dividido, g, 3);
+        ParallelDDA tarefa4 = new ParallelDDA(p2dividido, p2, g, 4);
 
         tarefa1.start();
         tarefa2.start();
+        tarefa3.start();
+        tarefa4.start();
 
         try {
             tarefa1.join();
             tarefa2.join();
+            tarefa3.join();
+            tarefa4.join();
         } catch (InterruptedException ex) {
         }
-        System.out.println("[DDA] : [parte1] [p1]" + tarefa1.getP1() +"[parte1] [p1]" + tarefa1.getP2());
-        System.out.println("[DDA] : [parte2] [p1]" + tarefa2.getP1() +"[parte1] [p1]" + tarefa2.getP2());
+        System.out.println("[DDA] : [parte1] [p1]" + tarefa1.getP1() + "[parte1] [p1]" + tarefa1.getP2());
+        System.out.println("[DDA] : [parte2] [p1]" + tarefa2.getP1() + "[parte1] [p1]" + tarefa2.getP2());
 
     }
 
-    
     //caso especifico para retangulo
     public static void plot(Point p1, Point p2, Graphics2D g) {
         g.setStroke(new BasicStroke(5));
         g.setPaint(Color.black);
 
-        
-        int mx = (p1.x + p2.x) / 2;
-        int my = (p1.y + p2.y) / 2;
-        pmedio = new Point(mx, my);
-        
-        ParallelDDA tarefa1 = new ParallelDDA(p1, pmedio, g);
-        ParallelDDA tarefa2 = new ParallelDDA(pmedio, p2, g);
+        pmedio = new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+        p1dividido = new Point((p1.x + pmedio.x) / 2, (p1.y + pmedio.y) / 2);
+        p2dividido = new Point((pmedio.x + p2.x) / 2, (pmedio.y + p2.y) / 2);
+
+        ParallelDDA tarefa1 = new ParallelDDA(p1, p1dividido, g, 1);
+        ParallelDDA tarefa2 = new ParallelDDA(p1dividido, pmedio, g, 2);
+        ParallelDDA tarefa3 = new ParallelDDA(pmedio, p2dividido, g, 3);
+        ParallelDDA tarefa4 = new ParallelDDA(p2dividido, p2, g, 4);
 
         tarefa1.start();
         tarefa2.start();
+        tarefa3.start();
+        tarefa4.start();
 
         try {
             tarefa1.join();
             tarefa2.join();
+            tarefa3.join();
+            tarefa4.join();
         } catch (InterruptedException ex) {
         }
-        
+
     }
 }

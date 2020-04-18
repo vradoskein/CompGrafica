a627b0edfbee6c8dadbaa0b678c76c81f2b78217
vradoskein <joaorener@gmail.com>
2020-04-18 14:47:21 -0300

    Parallelizing DDA, and rectangles ploting.

--- Paint/src/plots/DDA.java
+++ Paint/src/plots/DDA.java
@@ -13,39 +13,62 @@ import obj.Objeto;
  * @author Pedro
  */
 public class DDA {
+
+    static Point p1, p2, pmedio;
+
     public static void plot(Objeto o, Graphics2D g) {
         g.setStroke(new BasicStroke(5));
         g.setPaint(Color.black);
-        
-        Point p1, p2;
+
         p1 = o.getPontos().get(0);
         p2 = o.getPontos().get(1);
+        System.out.println("[DDA PLOTING] [p1] :" + p1 + "[p2]" + p2);
+
+        int mx = (p1.x + p2.x) / 2;
+        int my = (p1.y + p2.y) / 2;
+        pmedio = new Point(mx, my);
         
-        plot(p1, p2, g);
+        System.out.println("[DDA PLOTING] [PONTO MEDIO] :" + pmedio);
+        //plot(p1, p2, g);
+
+        ParallelDDA tarefa1 = new ParallelDDA(p1, pmedio, g);
+        ParallelDDA tarefa2 = new ParallelDDA(pmedio, p2, g);
+
+        tarefa1.start();
+        tarefa2.start();
+
+        try {
+            tarefa1.join();
+            tarefa2.join();
+        } catch (InterruptedException ex) {
+        }
+        System.out.println("[DDA] : [parte1] [p1]" + tarefa1.getP1() +"[parte1] [p1]" + tarefa1.getP2());
+        System.out.println("[DDA] : [parte2] [p1]" + tarefa2.getP1() +"[parte1] [p1]" + tarefa2.getP2());
+
     }
+
     
+    //caso especifico para retangulo
     public static void plot(Point p1, Point p2, Graphics2D g) {
         g.setStroke(new BasicStroke(5));
         g.setPaint(Color.black);
+
         
-        int dx = p2.x - p1.x;
-        int dy = p2.y - p1.y;
+        int mx = (p1.x + p2.x) / 2;
+        int my = (p1.y + p2.y) / 2;
+        pmedio = new Point(mx, my);
+        
+        ParallelDDA tarefa1 = new ParallelDDA(p1, pmedio, g);
+        ParallelDDA tarefa2 = new ParallelDDA(pmedio, p2, g);
 
-        // calculate steps required for generating pixels
-        int steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
+        tarefa1.start();
+        tarefa2.start();
 
-        // calculate increment in x & y for each steps
-        double Xinc = dx / (double) steps;
-        double Yinc = dy / (double) steps;
-
-        // Put pixel for each step
-        double X = p1.x;
-        double Y = p1.y;
-        for (int i = 0; i <= steps; i++)
-        {
-            g.draw(new Line2D.Double(X, Y, X, Y));
-            X += Xinc;
-            Y += Yinc;
+        try {
+            tarefa1.join();
+            tarefa2.join();
+        } catch (InterruptedException ex) {
         }
+        
     }
 }
